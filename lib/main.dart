1ad559bf364958c9e89a938f4e82685c985117e4
GabrielAdelino <gabrielbradelino@gmail.com>
2024-08-21 12:12:40 -0300

    Adequacões de código

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,12 +13,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
+      home: const LoginScreen(),
     );
   }
 }
 
 class LoginScreen extends StatefulWidget {
+    const LoginScreen({super.key});
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
